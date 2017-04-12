8c5593df01fa6f2f875473c9a4f51705929b0ef1
unknown <>
2017-04-12 23:39:19 +0800

    编程之美练习

--- Beauty_of_Programming/chapter_2/main.cpp
+++ Beauty_of_Programming/chapter_2/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <assert.h>
+#include <limits.h> 
 using namespace std;
 /*
 任意给定一个正整数N，求一个最小的正整数M（M>1），
@@ -45,9 +47,97 @@ vector<int> getMinM(unsigned int N) {
 		}
 	}
 }
+
+/*
+一个有N个整数元素的一维数组，求子数组（相邻的一段）之和的最大值。
+思路：前i个元素和如果小于0，则对后面的相加没有帮助 
+*/
+int getMaxSubarray(vector<int> nums) {
+	int len = nums.size();
+	assert(len > 0);
+	int ret = INT_MIN;
+	int tmp = 0;
+	for (int i = 0; i < len; i++) {
+		tmp += nums[i];
+		if (tmp > ret) {
+			ret = tmp;
+		}
+		if (tmp < 0) {
+			tmp = 0;
+		}
+	}
+	return ret;
+}
+
+/*
+有一个无序的，元素个数为2n的正整数数组，要求：
+如何能把这个数组分割为元素个数为n的两个数组，并使两个子数组的和最接近  
+*/
+class Node{
+	public:
+		Node() {};
+		Node(Node* n) {
+			for (int i = 0; i < n->list.size(); i++) {
+				this->list.push_back(n->list[i]);
+			}
+			this->sum = n->sum;
+		};
+		Node(int data) {
+			this->list.push_back(data);
+			this->sum = data;
+		}
+		void addValue(int target) {
+			this->list.push_back(target);
+			this->sum += target;
+		}
+		vector<int> list;
+		int sum;
+};
+Node getMindiffSubarray(vector<int> nums) {
+	int len = nums.size();
+	unsigned int sum = 0;
+	vector<Node> sumCnt[len];
+	for (int i = 0; i < len; i++) {
+		sum += nums[i];
+		int target = nums[i];
+		for (int z = i; z >= 0; z--) {
+			vector<Node> curList = sumCnt[z];
+			if (z == 0) {
+				Node node(target);
+				curList.push_back(node);
+			} else {
+				vector<Node> prenodeList = sumCnt[z - 1];
+				for (int j = 0; j < prenodeList.size(); j++) {
+					Node n = prenodeList[j];
+					Node newNode((Node*)&n);
+					newNode.addValue(target);
+					curList.push_back(newNode);
+				}
+			}
+			sumCnt[z] = curList;
+		}
+	}
+	
+	int in = (len >> 1) - 1;
+	vector<Node> targetList = sumCnt[in];
+	int halfSum = sum >> 1;
+	int tmpDiff = INT_MAX;
+	Node targetNode;
+	for (int q = 0; q < targetList.size(); q++) {
+		Node tmpNode = targetList[q];
+		if (abs(halfSum - tmpNode.sum) < tmpDiff) {
+			tmpDiff = abs(halfSum - tmpNode.sum);
+			targetNode = tmpNode;
+		}
+	}
+	return targetNode;
+}
+
 int main(){
-	int N = 3;
-	vector<int> vec = getMinM(N);
-	for (int i = 0; i < vec.size(); i++ )
-	cout<<vec[i]<<endl;
+	int N[] = {1,3,5,2,17,10,18,30};
+	vector<int> vec(N, N+sizeof(N)/sizeof(int));
+	Node ret = getMindiffSubarray(vec);
+	for (int i = 0; i < ret.list.size(); i++) {
+		cout<<ret.list[i]<<endl;
+	} 
 }
